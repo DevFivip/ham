34015c11815935921dcfb61cc1cb476ce10f149c
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-06-01 16:14:24 -0500

    fix: disable seed groups

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(
             [UserSeeder::class, LocationSeeder::class, CategoriesSeeder::class]
         );
-     \App\Models\Group::factory(1000)->create();
+     //\App\Models\Group::factory(1000)->create();
     }
 }
