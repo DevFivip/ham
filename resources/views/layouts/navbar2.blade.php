145238f4dbeddb76ba141aa5f69b8720de15ac4f
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-06-02 12:49:51 -0500

    feat

--- resources/views/layouts/navbar2.blade.php
+++ resources/views/layouts/navbar2.blade.php
@@ -1,7 +1,10 @@
 <nav class="bg-blue-600 border-gray-200 px-2 sm:px-4 py-2.5">
-    <div class="container flex flex-wrap justify-between items-center mx-auto">
+    <div x-data="{
+                     dropdownOpen2: false
+                    }
+                    " @click.outside="dropdownOpen2 = false" class="container flex flex-wrap justify-between items-center mx-auto">
 
-        <a href="/{{app()->getLocale()}}" class="flex items-center">
+        <a href="/{{app()->getLocale()}}" class="flex items-center hidden md:block">
             <img src="/assets/images/onlyham-logo.png" class="mr-3 h-6 sm:h-9" alt="Logo OnlyHam">
         </a>
 
@@ -27,7 +30,7 @@
                     {
                         dropdownOpen: false
                     }
-                    " @click.outside="dropdownOpen = false" class="relative inline-block ">
+                    " @click.outside="dropdownOpen = false" class="relative inline-block">
                 <button @click="dropdownOpen = !dropdownOpen" class="flex items-center rounded py-3 px-5 text-base font-semibold text-white">
                     <span class="fi fi-{{app()->getLocale() === 'en' ? 'us' : app()->getLocale() }} "></span>
                 </button>
@@ -46,7 +49,7 @@
                 </div>
             </div>
 
-            <button type="button" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-gray-500  rounded-lg text-sm p-2.5 mr-1">
+            <button @click="dropdownOpen2 = !dropdownOpen2" type="button" data-collapse-toggle="mobile-menu-3" aria-controls="mobile-menu-3" aria-expanded="false" class="md:hidden text-white  rounded-lg text-sm p-2.5 mr-1">
                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                 </svg>
@@ -62,7 +65,7 @@
                     <input type="text" id="search-navbar" name="name" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 " placeholder="{{__('Search')}}...">
                 </form>
             </div>
-            <button data-collapse-toggle="mobile-menu-3" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-3" aria-expanded="false">
+            <button @click="dropdownOpen2 = !dropdownOpen2" data-collapse-toggle="mobile-menu-3" type="button" class="inline-flex items-center p-2 text-sm text-white rounded-lg md:hidden hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-3" aria-expanded="false">
                 <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
                 </svg>
@@ -73,29 +76,34 @@
         </div>
 
 
-        <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-3">
+        <div :class="dropdownOpen2 ? 'block' : 'hidden'" class="justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-3">
             <div class="relative mt-3 md:hidden">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
                     </svg>
                 </div>
-                <input type="text" id="search-navbar" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500" placeholder="Search...">
+                <form action="/{{app()->getLocale()}}/search">
+                    <input type="text" id="search-navbar" name="name" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500" placeholder="{{__('Search')}}...">
+                </form>
             </div>
 
 
             <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                <li>
+                    <a href="/{{app()->getLocale()}}/" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-blue-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">
+                        {{__('Home')}}
+                    </a>
+                </li>
 
                 @foreach($redesSociales as $red)
                 <li>
-                    <a href="/{{app()->getLocale() }}/{{$red->name}}" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white">
+                    <a href="/{{app()->getLocale() }}/{{$red->name}}" class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-blue-500 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">
                         {{$red->name}}
                     </a>
                 </li>
                 @endforeach
-
             </ul>
-
         </div>
 
 
