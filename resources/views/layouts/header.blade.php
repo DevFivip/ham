1ea5574d0c024d77b8dfeb68862ef3e5ebfc02cc
Adolfo Gabazutt <adolfo.gbztt@gmail.com>
2022-06-17 13:56:52 -0500

    feat: add microsoft clarity

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,6 +38,15 @@
 </script>
 <!-- End Google Tag Manager -->
 
+<!--Clarity Microsoft-->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "cdw0pz2ks0");
+</script>
+
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-5E1F693QBP"></script>
